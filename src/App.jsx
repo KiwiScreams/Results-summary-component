93004bc7efd2aeee22812d5a08f73728994acd55
KiwiScreams <screamskiwi3@gmail.com>
2024-08-03 22:24:09 +0400

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,70 @@
+import image_1 from "./assets/images/icon-reaction.svg";
+import image_2 from "./assets/images/icon-memory.svg";
+import image_3 from "./assets/images/icon-verbal.svg";
+import image_4 from "./assets/images/icon-visual.svg";
+
 function App() {
   return (
     <>
       <div className="container flex">
         <section className="result">
-          <h3>Your result</h3> <div className="circle">0</div>
-          <p>of 100</p> <b>Great</b> <p>Your score text here</p>
+          <h3>Your result</h3>
+          <div className="circle">
+            <div className="circle-box">
+              <p>0</p>
+              <p>of 100</p>
+            </div>
+          </div>
+          <b>Great</b>
+          <p className="text">
+            Your performance exceed 65% of the people conducting the test here!
+          </p>
         </section>
         <section className="summary">
           <h3>Summary</h3>
-          <div className="rows">
+          <div className="rows flex">
             <div className="row flex">
-              <div className="left-cont">
-                <img src="" />
+              <div className="left-cont flex">
+                <img src={image_1} />
                 <b>Reaction</b>
               </div>
 
               <div className="right-cont">
                 <p>
-                  <b>92</b>/100
+                  <b>92</b>/ 100
                 </p>
               </div>
             </div>
             <div className="row flex">
               <div className="left-cont">
-                <img src="" />
+                <img src={image_2} />
                 <b>Memory</b>
               </div>
               <div className="right-cont">
                 <p>
-                  <b>92</b>/100
+                  <b>92</b>/ 100
                 </p>
               </div>
             </div>
             <div className="row flex">
               <div className="left-cont">
-                <img src="" />
+                <img src={image_3} />
                 <b>Verbal</b>
               </div>
               <div className="right-cont">
                 <p>
-                  <b>92</b>/100
+                  <b>92</b>/ 100
                 </p>
               </div>
             </div>
             <div className="row flex">
               <div className="left-cont">
-                <img src="" />
+                <img src={image_4} />
                 <b>Visual</b>
               </div>
               <div className="right-cont">
                 <p>
-                  <b>92</b>/100
+                  <b>92</b>/ 100
                 </p>
               </div>
             </div>
