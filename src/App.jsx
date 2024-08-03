e6261f07e22d3b196b2fee6562919dcee633ebfc
KiwiScreams <screamskiwi3@gmail.com>
2024-08-03 21:48:02 +0400

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,61 @@
 function App() {
   return (
     <>
-      <div class="container flex">
-        <section class="result">
-          <h3>Your result</h3> <div class="circle">0</div>
+      <div className="container flex">
+        <section className="result">
+          <h3>Your result</h3> <div className="circle">0</div>
           <p>of 100</p> <b>Great</b> <p>Your score text here</p>
         </section>
-        <section class="sumary">
+        <section className="summary">
           <h3>Summary</h3>
-          <div class="rows">
-            <div class="row flex">
-              <div class="left-cont">
+          <div className="rows">
+            <div className="row flex">
+              <div className="left-cont">
                 <img src="" />
                 <b>Reaction</b>
               </div>
 
-              <div class="right-cont">
+              <div className="right-cont">
                 <p>
                   <b>92</b>/100
                 </p>
               </div>
             </div>
-            <div class="row flex">
-              <div class="left-cont">
+            <div className="row flex">
+              <div className="left-cont">
                 <img src="" />
                 <b>Memory</b>
               </div>
-              <div class="right-cont">
+              <div className="right-cont">
                 <p>
                   <b>92</b>/100
                 </p>
               </div>
             </div>
-            <div class="row flex">
-              <div class="left-cont">
+            <div className="row flex">
+              <div className="left-cont">
                 <img src="" />
                 <b>Verbal</b>
               </div>
-              <div class="right-cont">
+              <div className="right-cont">
                 <p>
                   <b>92</b>/100
                 </p>
               </div>
             </div>
-            <div class="row flex">
-              <div class="left-cont">
+            <div className="row flex">
+              <div className="left-cont">
                 <img src="" />
                 <b>Visual</b>
               </div>
-              <div class="right-cont">
+              <div className="right-cont">
                 <p>
                   <b>92</b>/100
                 </p>
               </div>
             </div>
           </div>
-          <p class="btn">Continue</p>
+          <p className="btn">Continue</p>
         </section>
       </div>
     </>
